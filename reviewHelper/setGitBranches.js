95208cccd78ec2525153886545eeb10384ab9cdb
Jack McCown <jmccown@cbinsights.com>
2016-09-06 15:44:06 -0400

    Better method of checking repo status

--- reviewHelper/setGitBranches.js
+++ reviewHelper/setGitBranches.js
@@ -2,44 +2,42 @@ const shell = require('shelljs')
 const path = require('path')
 
 runCmd = (cmd) => {
-    return shell.exec(cmd, {silent:true}).toString()
+  return shell.exec(cmd, {silent:true})
 }
 
 listDirs = (dir) => {
-    shell.cd(dir)
-    return runCmd("ls").split("\n")
+  shell.cd(dir)
+  return runCmd("ls").toString().split("\n")
 }
 
 const getWorkingDir = (repo) => {
-    if (!(process.env.CBI_ROOT)) {
-        throw new Error("Need to set CBI_ROOT environment var")
-    }
-    r = process.env.CBI_ROOT
-    if (!repo in listDirs(r)) {
-        throw new Error(`Couldn't find working dir for ${repo}`)
-    }
-    workingDir = path.join(r,repo)
-    shell.cd(workingDir)
-    return workingDir
+  if (!(process.env.CBI_ROOT)) {
+    throw new Error("Need to set CBI_ROOT environment var")
+  }
+  r = process.env.CBI_ROOT
+  if (!repo in listDirs(r)) {
+    throw new Error(`Couldn't find working dir for ${repo}`)
+  }
+  workingDir = path.join(r,repo)
+  shell.cd(workingDir)
+  return workingDir
 }
 
 const checkRepoReady = () => {
-    status = runCmd('git status')
-    currStatus = status.split("\n")[1]
-    readyMsg = /[nothing to commit, working directory clean]/
-    if (!currStatus.match(readyMsg)) {
-        throw new Error(`Stopping due to uncommitted changes in ${repo}`)
-    }
+  status = runCmd('git diff-index --quiet HEAD --')
+  if (status.code !== 0) {
+    throw new Error(`Stopping due to uncommitted changes in ${repo}`)
+  }
 
 }
 
 const repoSetup = (repo, branch) => {
-    const wd = getWorkingDir(repo)
-    checkRepoReady()
-    runCmd('git fetch --all')
-    runCmd(`git checkout ${branch}`)
-    runCmd('git pull')
-    return wd
+  const wd = getWorkingDir(repo)
+  checkRepoReady()
+  runCmd('git fetch --all')
+  runCmd(`git checkout ${branch}`)
+  runCmd('git pull')
+  return wd
 }
 
 module.exports = repoSetup
