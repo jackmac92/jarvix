90497f714f3da818016c17674aebdad2f12a24a1
Jack McCown <jmccown@cbinsights.com>
2016-09-07 16:48:23 -0400

    Update readme and semicolons

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 Setup
 sudo chmod +x install.sh
-set CBI_ROOT environment variable to the dir containing local repos
 sudo ./install.sh
+set CBI_ROOT environment variable to the dir containing local repos
 ./jarvixServer.js
-
 Install chrome extension
 
-If jarvixServer exits at any point you'll need to reload the chrome extension
+The jarvixServer must be running before the chrome extension begins running. If jarvixServer exits at any point, you'll need to reload the chrome extension
 Chrome > Settings > Extensions
 Find the jarvix extension listing and click background page, which opens a chrome console for the extension, and cmd+R to reload (working on a fix)
+
+What it does
+Finds test-runner screenshots on jenkins and dev.test.cbinsights.com, downloads and opens them
+Finds branches you are reviewing on crucible to be opened locally
+
+TODO
+Finish implementing windowSetup for reviewHandler, so that repos open on the reviewed branch in the editor of your choice
+Allow saving file changes to a path, which could then be sent to the author of the reviewed code
--- handlers/jenkinsHandler.js
+++ handlers/jenkinsHandler.js
@@ -1,34 +1,36 @@
 #!/usr/bin/env node
 const readline = require('readline-sync');
-const shell = require('shelljs')
-const screenshotGrabber = require('../utils/downloadScreenShot.js')
-const winSetup = require('../utils/newTermWindowSetup.js')
+const shell = require('shelljs');
+const screenshotGrabber = require('../utils/downloadScreenShot.js');
+const winSetup = require('../utils/newTermWindowSetup.js');
 
-const setupInfo = winSetup(process.argv[2])
+const setupInfo = winSetup(process.argv[2]);
 
-const args = setupInfo[0]
-const tmpDir = setupInfo[1]
+const screenshotsInfo = setupInfo[0];
+const tmpDir = setupInfo[1];
 
-args.tests.forEach( (el, i) => {
-  console.log()
-  console.log(`${i}: ${el.testName}`)
+screenshotsInfo.tests.forEach( (el, i) => {
+  console.log(`${i}: ${el.testName}`);
 })
 
-input = readline.question("Enter space separated numbers for individual screenshots, no input gets all")
+input = readline.question("Enter space separated numbers for individual screenshots, no input gets all");
 
 
-pics = (input.length === 0) ? args.tests
-                            : input.split(/\s/).map(n => args.tests[n])
+if (input.length === 0) {
+  pics = args.tests;
+} else {
+  pics = input.split(/\s/).map(n => args.tests[n]);
+}
 
-console.log(`Fetching ${pics.length} screenshots`)
+console.log(`Fetching ${pics.length} screenshots`);
 
 Promise.all(
     pics.map(p => screenshotGrabber(args.branch, p.screenshot, tmpDir))
   ).then(values => {
-    console.log("Done")
-    input = readline.question("Press enter to delete screenshots and exit")
-    result = shell.exec(`rm -rf ${tmpDir}`)
+    console.log("Done");
+    input = readline.question("Press enter to delete screenshots and exit");
+    result = shell.exec(`rm -rf ${tmpDir}`);
     if (result.code !== 0) {
-      console.log(result.stderr)
+      console.log(result.stderr);
     }
 }).catch(reason => console.log(reason))
