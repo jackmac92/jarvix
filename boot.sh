3552b6c17dc695d6f6c4aa84f2d9bad7a1ff8645
Jack McCown <jmccown@cbinsights.com>
2017-06-10 12:55:06 -0400

    fix boot script

--- boot.sh
+++ boot.sh
@@ -1,6 +1,5 @@
 #! /bin/bash -ex
 
-
 while getopts 'h:p:c' flag; do
   case "${flag}" in
     p) devPort="${OPTARG}" ;;
@@ -12,15 +11,18 @@ done
 devPort=${devPort:-7442}
 
 run() {
-    docker run -p "$devPort":"$devPort" jarvix-host
+    docker run -d \
+        --name jarvix \
+        -p "$devPort":"$devPort" \
+        jarvix-host
 }
 
 build() {
     docker build . -t jarvix-host --build-arg APPPORT="$devPort"
 }
 
-if [[ $shouldBuild ]]; then
+if [[ $shouldBuild || $(docker images | grep -c jarvix-host) -eq 0 ]]; then
     build
 fi
 
-run
+run && docker logs -f jarvix
